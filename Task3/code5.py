af4e5a42c1179de6d72b3eca2f06faa0a37d7ee3
Aya Hamada Ahmed <139050447+ayahamadaahmed@users.noreply.github.com>
2025-10-12 18:10:19 +0300

    Add files via upload

---
+++ Task3/code5.py
@@ -0,0 +1,17 @@
+# def is_prime(n):
+#     if n < 2:
+#         return False
+#     for k in range(2, n):
+#         if n % k == 0:
+#             return False
+#     return True
+
+# def is_prime(n):
+#     if n < 2:
+#         return False
+#     for k in range(2, n):
+#         if n % k == 0:
+#             return False
+#     return True
+
+# def is_prime(n):
